61679507b0fb503e7957ad2eb052ffd1ad2ba40b
sunzhiqiang <sun1358742@163.com>
2021-06-03 18:09:02 +0800

    init project

---
+++ src/Lee.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Lee extends Component<any, any> {
+  render() {
+    return (
+      <div>
+        测试
+        {this.props.name}
+      </div>
+    )
+  }
+}
+
+export default Lee
