46767dc549e1b66d41b4bf894327771b3a0194c2
sunzhiqiang <sun1358742@163.com>
2021-06-04 19:02:40 +0800

    测试 react 点击按钮改变宽度

--- src/components/lean/Learn_jsx.tsx
+++ src/components/lean/Learn_jsx.tsx
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
 import bg from '../../assets/images/pic.jpeg'
 import './index.css'
-class LearnJsx extends Component<any,any> {
+interface Istate {
+    width: number
+}
+
+class LearnJsx extends Component<any,Istate> {
 
     constructor(props: any, context: any) {
         super(props, context);
         this.state = {
-            img: bg,
+            width:500
         }
+    }
+    //改变宽度
+    change = () => {
+        console.log('111');
+        this.setState({
+            width:this.state.width + 10
+        })
     }
     render() {
         return (
             <div>
-                <img src={this.state.img} alt=""/>
+                <img src={bg} alt=""/>
                 <hr/>
-                <img src={require('../../assets/images/pic.jpeg').default} alt=""/>
+                <img src={require('../../assets/images/pic.jpeg').default} alt=""/>;
                 <hr/>
-                <div className='title' style={{backgroundColor:"pink"}}>测试css文件</div>
+                <div className='title' style={{backgroundColor:"pink",width:`${this.state.width}px`}}>测试css文件</div>
+                <hr/>
+                <button onClick={this.change}>改变宽度</button>
             </div>
         );
     }
