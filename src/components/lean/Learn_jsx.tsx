0ae493c0f477779a89523bb359d3410f4c620787
sunzhiqiang <sun1358742@163.com>
2021-06-04 19:32:10 +0800

    测试 react 生命周期

--- src/components/lean/Learn_jsx.tsx
+++ src/components/lean/Learn_jsx.tsx
@@ -9,10 +9,28 @@ class LearnJsx extends Component<any,Istate> {
 
     constructor(props: any, context: any) {
         super(props, context);
+        console.log('constructor,1');
         this.state = {
             width:500
         }
+
     }
+    componentDidMount(): void {
+        console.log('componentDidMount,挂载时触发 3')
+    }
+
+    componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<Istate>, snapshot?: any): void {
+        console.log('componentDidUpdate,页面更新之后触发')
+    }
+
+    shouldComponentUpdate(nextProps: Readonly<any>, nextState: Readonly<Istate>, nextContext: any): boolean {
+        return true
+    }
+
+    componentWillUnmount(): void {
+        console.log('componentWillUnmount 页面卸载时候触发 4')
+    }
+
     //改变宽度
     change = () => {
         console.log('111');
@@ -25,6 +43,7 @@ class LearnJsx extends Component<any,Istate> {
         }))
     };
     render() {
+        console.log('render 2');
         return (
             <div>
                 <img src={bg} alt=""/>
