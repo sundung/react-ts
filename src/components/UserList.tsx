eb9f8ba70d99bb04db46b99de1f56a346699d4b3
sundung <sun1358742@163.com>
2021-06-11 18:57:18 +0800

    table表格添加操作列

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -37,14 +37,6 @@ class UserList extends Component <any, Istate> {
                     <Table.Column title={'id'} dataIndex={'id'} />
 
                     <Table.Column title={'用户名'} dataIndex={'name'}/>
-
-                    <Table.Column title={'管理'}
-                        render={(user:IUser) => (
-                            <Space>
-                                <Button type='primary'>编辑</Button>
-                                <Button type='primary' danger>删除</Button>
-                            </Space>
-                    )}/>
                 </Table>
             </div>
         );
